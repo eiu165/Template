c0eba39a65ca821cf49fede649431d712c9d08fc
eiu165 <eiu165@gmail.com>
2012-09-20 14:20:18 -0500

    added namespace

--- Web/Areas/SecurityGuard/Models/RegisterViewModel.cs
+++ Web/Areas/SecurityGuard/Models/RegisterViewModel.cs
@@ -22,7 +22,7 @@ namespace Web.Areas.SecurityGuard.ViewModels
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
 
         [Display(Name = "Secret Question")]
