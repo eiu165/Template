4dbb9de7501b0a93485b595868e558bd19be7220
eiu165 <eiu165@gmail.com>
2012-09-20 15:24:22 -0500

    remove aspx 's

--- Web/Data/Migration/Migration.sql
+++ Web/Data/Migration/Migration.sql
@@ -1,4 +1,25 @@
-﻿print '----------------'
+﻿
+use Template
 
 
+ 
+if not exists (select * from webpages_Roles where RoleName = 'SecurityGuard')
+begin
+	insert into webpages_Roles ([RoleName]) values ('SecurityGuard')
+end 
+else  
+begin
+	print 'already there'
+end
 
+
+if not exists (select * from webpages_UsersInRoles where [UserId] = 1 and [RoleId] = 1)
+begin
+	insert into webpages_UsersInRoles ([UserId], [RoleId]) values (1,1)
+end 
+else  
+begin
+	print 'already there'
+end 
+
+ 
