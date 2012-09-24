2b7287f1514ba29b38856db75b312162fc206f43
eiu165 <eiu165@gmail.com>
2012-09-24 11:08:50 -0500

    added admin area

---
+++ Web/Areas/Admin/Controllers/UserRolesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Areas.Admin.Controllers
+{
+    public class UserRolesController : Controller
+    {
+        //
+        // GET: /Admin/UserRoles/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
